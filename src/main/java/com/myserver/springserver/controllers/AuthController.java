66fe537d67b1ee494a1c03f47563318b04f5984b
Pavel Chekalin <grokki91@gmail.com>
2025-02-07 17:02:32 +0300

    add endpoint /api in auth

--- src/main/java/com/myserver/springserver/controllers/AuthController.java
+++ src/main/java/com/myserver/springserver/controllers/AuthController.java
@@ -15,7 +15,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/")
+@RequestMapping("/api")
 @AllArgsConstructor
 public class AuthController {
     @Autowired
