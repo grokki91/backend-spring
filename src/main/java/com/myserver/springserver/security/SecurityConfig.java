0a22067913d0be168ea6966b8c1decfa10fce8fc
Pavel Chekalin <grokki91@gmail.com>
2025-02-20 19:52:39 +0300

    change security config

--- src/main/java/com/myserver/springserver/security/SecurityConfig.java
+++ src/main/java/com/myserver/springserver/security/SecurityConfig.java
@@ -44,8 +44,7 @@ public class SecurityConfig {
                 .csrf(AbstractHttpConfigurer::disable)
                 .sessionManagement((session) -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
                 .authorizeHttpRequests(auth -> auth
-                        .requestMatchers(HttpMethod.OPTIONS, "/api/**").permitAll()
-                        .requestMatchers("/signup", "/login").permitAll()
+                        .requestMatchers("/api/signup", "/api/login").permitAll()
                         .requestMatchers("/v3/api-docs/**", "/swagger-ui/**", "/swagger-resources/*").permitAll()
                         .anyRequest().authenticated())
                 .exceptionHandling((exceptions) -> exceptions
